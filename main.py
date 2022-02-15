b493ae47c03848c53836705b08d9ed812c2395ef
AvaRomeo <maartjekilsdonk@gmail.com>
2022-02-15 15:06:07 +0100

    Opdracht 1 + file directory

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import time
 import os
 
 #Read check values
-file1 = open("rules/age.txt", "r")
+file1 = open("C:/xampp/htdocs/radius_college/GitHub/rollercoaster-check/rules/age.txt", "r")
 age_check = int(file1.read())
 file1.close()
 
-file2 = open("rules/height.txt", "r")
+file2 = open("C:/xampp/htdocs/radius_college/GitHub/rollercoaster-check/rules/height.txt", "r")
 height_check = int(file2.read())
-file1.close()
+file2.close()
 
 running = True
 while running:
